62166a5a7ab351966028a25788766424bf4f0098
NetzFinance <161255993+NetzFinance@users.noreply.github.com>
2024-12-17 22:04:40 +0000

    Create basewnetz.js

---
+++ basewnetz.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const { execSync } = require("child_process");
+
+if (!process.env.TW_SECRET_KEY) {
+  console.error("PRIVATE_KEY is not set in the .env file");
+  process.exit(1);
+}
+
+execSync(
+  `npx thirdweb@latest deploy -cn BaseBridgedWNETZ -k ${process.env.TW_SECRET_KEY}`,
+  {
+    stdio: "inherit",
+  },
+);
