334f53fd2a831f392f72835f7b1816de09bd39d3
NetzFinance <161255993+NetzFinance@users.noreply.github.com>
2024-12-17 20:23:55 +0000

    Update COMPILED_LINKS.ts

--- COMPILED_LINKS.ts
+++ COMPILED_LINKS.ts
@@ -1,8 +1,8 @@
 export const BRIDGED_WNETZ_DEPLOY =
-  "https://thirdweb.com/contracts/deploy/QmbExtJBMdoNiAEWn4WaV2g1kq3jWsVxV3F265hQsVUTgU";
+  "https://thirdweb.com/contracts/deploy/QmbfC4ELTgLtCZoqip5j3WJv371qAbFDmYQixrF3UtngRP";
 
 export const WNETZ_BRIDGE_DEPLOY =
-  "https://thirdweb.com/contracts/deploy/QmTuQdNVwqqkxFP3ksaZZoeRfwKDZq67R4mdVRYiaVjpHr";
+  "https://thirdweb.com/contracts/deploy/QmPvXU8vKPzUe6TnVd23rLBFhjPmDb7CUDcujatbQ9c2Jx";
 
 export const FAUCET_DEPLOY =
   "https://thirdweb.com/contracts/deploy/QmcrkRsPAz8zXsNDj7tLhsXR2dbZXttpadLd59oE1LhgTT";
